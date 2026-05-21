967535e87c92d5633b45a153694e8061cb98d11f
michellecruzcobos-svg <michelle.cruzcobos@gmail.com>
2026-05-20 20:06:07 -0500

    Add files via upload
    SENSOR DE TEMPERATURA Y HUMEDAD

---
+++ PRACTICA 2 INTENTO 1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace PRACTICA_2_INTENTO_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.temperaturaLabel = new System.Windows.Forms.Label();
+            this.humedadLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(76, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 145);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(76, 263);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(136, 145);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // temperaturaLabel
+            // 
+            this.temperaturaLabel.AutoSize = true;
+            this.temperaturaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.temperaturaLabel.Location = new System.Drawing.Point(33, 209);
+            this.temperaturaLabel.Name = "temperaturaLabel";
+            this.temperaturaLabel.Size = new System.Drawing.Size(226, 32);
+            this.temperaturaLabel.TabIndex = 2;
+            this.temperaturaLabel.Text = "TEMPERATURA";
+            this.temperaturaLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // humedadLabel
+            // 
+            this.humedadLabel.AutoSize = true;
+            this.humedadLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.humedadLabel.Location = new System.Drawing.Point(70, 411);
+            this.humedadLabel.Name = "humedadLabel";
+            this.humedadLabel.Size = new System.Drawing.Size(155, 32);
+            this.humedadLabel.TabIndex = 3;
+            this.humedadLabel.Text = "HUMEDAD";
+            this.humedadLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(54, 474);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CONECTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(284, 63);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(480, 439);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(572, 530);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "SALIR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 592);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.humedadLabel);
+            this.Controls.Add(this.temperaturaLabel);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label temperaturaLabel;
+        private System.Windows.Forms.Label humedadLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PRACTICA 2 INTENTO 1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRACTICA_2_INTENTO_1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;       
+        bool puertoCerrado = false;  
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            serialPort = new SerialPort();
+            serialPort.PortName = "COM4"; 
+            serialPort.BaudRate = 9600;
+
+            serialPort.DataReceived += DataReceivedHandler; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                conectar();
+            }
+            else
+            {
+                noConectado();
+            }
+        }
+
+        private void conectar()
+        {
+            try
+            {
+                serialPort.Open();
+                puertoCerrado = false;
+                MessageBox.Show("Conectado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectar: " + ex.Message);
+            }
+        }
+
+        private void noConectado()
+        {
+            try
+            {
+                serialPort.Close();
+                puertoCerrado = true;
+                MessageBox.Show("Desconectado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al desconectar: " + ex.Message);
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = serialPort.ReadExisting();
+
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    listBox1.Items.Add(data);
+                }));
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
